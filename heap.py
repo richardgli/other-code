bda1fe6308ddaa9d64c573425fea94726e759939
richardgli <ric.li6374@gmail.com>
2024-10-20 09:21:34 -0400

    Update heap.py

--- heap.py
+++ heap.py
@@ -11,12 +11,27 @@ nums = [3, 2, 3, 1, 2, 4, 5, 5, 6], k = 4
 4
 '''
 
+'''
+Given an integer array nums and an integer k, return kth.
+Note that it is the kth largest element in the sorted order, 
+not the kth distinct element.
+Solve without sorting.
+
+nums = [3, 2, 1, 5, 6, 4], k = 2
+5
+
+nums = [3, 2, 3, 1, 2, 4, 5, 5, 6], k = 4
+4
+'''
+
 import heapq
 
 nums = list(map(int, input().split()))
 k = int(input())
+kl = nums[:k]
 
-heapq.heapify(nums)
-for i in range(len(nums) - k + 1):
-    curMax = heapq.heappop(nums)
-print(curMax)
+heapq.heapify(kl)
+for i in range(k, len(nums)):
+    if nums[i] > min(kl):
+        cur = heapq.heapreplace(kl, nums[i])
+print(kl[0])
