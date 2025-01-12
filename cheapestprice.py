77d21f0d056a0267b6b7d73069df78eb82f0562b
richardgli <ric.li6374@gmail.com>
2025-01-11 23:39:16 -0500

    Update cheapestprice.py

--- cheapestprice.py
+++ cheapestprice.py
@@ -20,29 +20,26 @@ flights = [[0, 1, 100], [1, 2, 100], [0, 2, 500]]
 src = 0
 dst = 2
 k = 0
+
 def cheapestPrice(n, flights, src, dst, k):
     adjList = defaultdict(list)
     changed = False
-    for i in range(n):
+    for i in range(len(flights)):
         fromCity = flights[i][0]
         toCity = flights[i][1]
         price = flights[i][2]
         adjList[fromCity].append((toCity, price))
     
     minPrice = ['inf'] * n
+    minPrice[src] = 0
     numStops = [-1] * n
+    
     for j in range(n - 1):
         changed = False
         for i in range(n):
-            if adjList[i]:
+            if adjList[i] and minPrice[i] != 'inf':
                 for toCity, price in adjList[i]:
-                    if minPrice[i] == 'inf' and minPrice[toCity] != 'inf':
-                        continue
-                    elif minPrice[toCity] == 'inf' and minPrice[i] == 'inf':
-                        minPrice[toCity] = price                   
-                        numStops[toCity] += 1
-                        changed = True
-                    elif minPrice[toCity] == 'inf' and minPrice[i] != 'inf':
+                    if minPrice[toCity] == 'inf' and minPrice[i] != 'inf':
                         minPrice[toCity] = price + minPrice[i]                  
                         numStops[toCity] += 1
                         changed = True
@@ -57,5 +54,3 @@ def cheapestPrice(n, flights, src, dst, k):
     return -1
 
 print(cheapestPrice(n, flights, src, dst, k))
-
-
